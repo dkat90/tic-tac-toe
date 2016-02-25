ec523b30189d53dcaa21b7ac1ea7d984db725c62
David Tambunan <dkat90@gmail.com>
2016-02-25 21:34:55 +0800

    added some JjQuery

--- js/js2.js
+++ js/js2.js
@@ -1,67 +1,50 @@
 function init() {
   var player = "X";
   var winner = null;
-  var grid = document.getElementsByClassName('grid');
 
-  //Finding out which are the players
   function startGame() {
     var player = "X";
     var winner = null;
-    if(Math.random() < 0.5) {
-      player = "O"//selecting random player when page refreshed
-    }
-    setMessage(player + " gets to start");
+    $('#message').text(player + " gets to start");
   }
 
-  function setMessage(msg) {
-    document.getElementById("message").innerHTML = msg;
-  }
+  $('.grid').on('click', playerClick)
 
-  //Enables us to click each grid and player selection
-  for (i=0; i<grid.length; i++) {
-    grid[i].addEventListener("click", playerClick, false);
-    console.log(grid[i])
-  }
   function playerClick() {
     if(winner !== null) {
-      setMessage(winner + " already won the game")
+      $('#message').text(winner + " already won the game")
     }
     else if(this.innerHTML === ""){
       this.innerHTML = player;
       switchTurn();
     }
     else {
-      setMessage("That square is already used you numpty.");
+      $('#message').text("That square is already used you numpty.");
     }
 
   }
-  //changing colors
-  for(i = 0; i < grid.length; i++) {
-    grid[i].addEventListener('click', changeBackground,false);
-  }
+  $('.grid').on('click', changeBackground);
 
   function changeBackground() {
     if (player === "X") {
-      this.style.background = "blue";
+      this.style.background = "#d99694";
     } else if (player === "O") {
-      this.style.background = "red";
+      this.style.background = "#b0e0e6";
     }
   }
 
-
-  //switch turn for player and ending game
   function switchTurn() {
     if(checkForWinner(player)) {
-      setMessage("Congratulations "+ player + " you win!....Now get on with life.")
+      alert("Congratulations "+ player + " you win!....Now get on with life.")
       winner = player;
     }
     else if (player === "X") {
       player = "O";
-      setMessage("It's " + player + " turn");
+      $('#message').text("It's " + player + " turn");
 
     } else {
       player = "X";
-      setMessage("It's " + player + " turn");
+      $('#message').text("It's " + player + " turn");
     }
   };
 
@@ -87,8 +70,6 @@ function init() {
         result = true;
       } else if (checkRow(3, 5, 7, move)) {
         result = true;
-      } else {
-        setMessage("No one wins..HAH!");
       }
        return result;
    }
@@ -107,20 +88,11 @@ function init() {
   }
   //clearing those damn boxes
 
-    var button = document.getElementById('clearButton');
-    function clearBox () {
-      console.log("clearBox");
-      for (i=0; i<grid.length; i++) {
-        grid[i].innerHTML = "";
-        grid[i].style.background="white";
-      }
-
-      player = "X";
-      winner = null;
-      setMessage("New Game noobs!");
-
-    }
-    button.addEventListener('click', clearBox, false);
+    $('#clearButton').on('click', function(){
+      console.log("board is cleared");
+      $('.grid').text("").css('background','white');
+      $('#message').text("Click on board to start!");
+    });
 
 
 
